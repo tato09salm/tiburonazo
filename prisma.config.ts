c0c01d8978a0c0fd6287f46074ffdf420357a9d5
Pandaman08 <pandaman010608@gmail.com>
2026-09-09 16:37:27 -0500

    fix: configuracion del seed en la configuracion de prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  migrations: {
+    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
+  },
   datasource: {
     url: process.env.DATABASE_URL,
   },
